9b6d05527ac868081e44b3ab14583c0be3154b26
td94 <quynhtrangdo94@gmail.com>
2019-01-09 00:44:35 -0800

    Setup Mongo server

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+import auth from './routes/auth';
 
 const app = express();
-
-app.post('/api/auth', (req,res) => {
-    res.status(400).json({ errors: { global: "Invalid credentials"}})
+app.use(bodyParser.json());
+mongoose.connect("mongodb://localhost/bookworm",{ useNewUrlParser: true }, (err) => {
+    if (err) {
+        console.log('failed to connect to mongo');
+        console.log('ERROR', err);
+        
+    } else {
+        console.log('Successfully connected to db');
+    }
 })
+
+app.use('/api/auth', auth);
+
 
 app.get('/*', (req,res) => {
     res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.listen(8080, () => console.log("running on localhost:8080"))
+app.listen(3000, () => console.log("running on localhost:3000"))
