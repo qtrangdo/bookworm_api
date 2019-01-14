a7e70080ee26db09bf15722d286f732dbd138cca
td94 <quynhtrangdo94@gmail.com>
2019-01-14 00:21:14 -0800

    Add mailer and confirmation responses

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import User from '../models/User';
 import parseError from '../utils/parseError';
+import { sendConfirmationEmail } from '../mailer';
 
 const router = express.Router();
 router.post('/', (req,res) => {
     const {email, password} = req.body.user;
     const user = new User({ email });
     user.setPassword(password);
+    user.setConfirmationToken();
     user.save()
-    .then(userRecord => res.json({ user: userRecord.toAuthJSON() }))
+    .then(userRecord => {
+      sendConfirmationEmail(userRecord);
+      res.json({ user: userRecord.toAuthJSON() })
+    })
       .catch(err => res.status(400).json({errors: parseError(err.errors)}))
 
 })
