73ad180132fe33c58aa65a872d0ea3a099ebd721
td94 <quynhtrangdo94@gmail.com>
2019-01-20 13:32:35 -0800

    Create action for reset password

--- src/models/User.js
+++ src/models/User.js
@@ -41,6 +41,10 @@ schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
     return `${process.env.HOST}/confirmation/${this.confirmationToken}`
 }
 
+schema.methods.generateResetPasswordLink = function generateResetPasswordLink() {
+    return `${process.env.HOST}/reset_password/${this.generateResetPasswordToken()}`
+}
+
 schema.methods.generateJWT = function generateJWT() {
     return jwt.sign(
         {
@@ -48,7 +52,17 @@ schema.methods.generateJWT = function generateJWT() {
             confirmed: this.confirmed
         },
         process.env.JWT_SECRET)
-}
+};
+
+schema.methods.generateResetPasswordToken = function generateResetPasswordToken() {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: '1s'}
+    );
+};
 
 schema.methods.toAuthJSON = function toAuthJSON() {
     return {
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import User from '../models/User';
-
+import { sendResetPasswordEmail } from '../mailer';
 
 const router = express.Router();
 
@@ -26,5 +26,16 @@ router.post('/confirmation', (req,res) => {
       user ? res.json({ user: user.toAuthJSON() }) : res.status(400).json({})
     )
   })
+
+router.post('/reset_password_request', (req,res) => {
+    User.findOne({email: req.body.email}).then(user => {
+        if (user) {
+            sendResetPasswordEmail(user);
+            res.json({});
+        } else {
+            res.status(400).json({ errors: {global: 'There is no user with such email'} });
+        }
+    })
+})
 
 export default router;
