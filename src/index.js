b7cb7b90bff4e1945603c742b2d71b14ae1e9996
td94 <quynhtrangdo94@gmail.com>
2019-01-21 16:50:54 -0800

    Change broken img link

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Promise from 'bluebird';
 
 import auth from './routes/auth';
 import users from './routes/users';
+import books from './routes/books';
 
 dotenv.config();
 const app = express();
@@ -24,6 +25,7 @@ mongoose.connect(process.env.MONGODB_URL,{ useNewUrlParser: true }, (err) => {
 
 app.use('/api/auth', auth);
 app.use('/api/users', users);
+app.use('/api/books', books);
 
 
 app.get('/*', (req,res) => {
